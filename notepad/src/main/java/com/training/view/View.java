70d47271be1fad0eaed393cefa191a577623c1f0
Zhuchkov <Zhuchkov.vlad@gmail.com>
2017-05-19 15:11:38 +0300

    remove useless modifiers

--- notepad/src/main/java/com/training/view/View.java
+++ notepad/src/main/java/com/training/view/View.java
@@ -4,14 +4,14 @@ import com.training.model.Note;
 
 public class View {
 
-	public static final String DATA_REQUEST = "info.note.creation";
-	public static final String INPUT_REQUEST_STRING = "request.input.string";
-	public static final String INPUT_WRONG_STRING = "error.input.string";
-	public static final String SAVE_REQUEST = "request.input.save";
-	public static final String WRONG_UNMERIC_INPUT = "error.input.numeric";
-	public static final String NICKNAME_OCCUPIED = "error.database.unick.nicknme";
-	public static final String DB_EXCEPTION = "error.database.unknown";
-	public static final String REQUEST_INPUT_LANGUAGE = "request.input.language";
+	public static  String DATA_REQUEST = "info.note.creation";
+	public static  String INPUT_REQUEST_STRING = "request.input.string";
+	public static  String INPUT_WRONG_STRING = "error.input.string";
+	public static  String SAVE_REQUEST = "request.input.save";
+	public static  String WRONG_UNMERIC_INPUT = "error.input.numeric";
+	public static  String NICKNAME_OCCUPIED = "error.database.unick.nicknme";
+	public static  String DB_EXCEPTION = "error.database.unknown";
+	public static  String REQUEST_INPUT_LANGUAGE = "request.input.language";
 
 	public void print(String... output) {
 		for (String s : output) {
